3f002698dc0ebe544c366ee7acc204443be4cd08
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-06-09 22:43:52 +0530

    Add files via upload

---
+++ Leet code 10 program 08-06-24/P1.py
@@ -0,0 +1,13 @@
+# Two Sum
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevmap = {} # val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevmap:
+                return [prevmap[diff], i]
+            prevmap[n] = i
+        return
---
+++ Leet code 10 program 09-06-24/P9.py
@@ -0,0 +1,8 @@
+# Single Number
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for n in nums:
+            result = n ^ result
+        return result
